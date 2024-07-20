6a2c20ea0afe23bc19979bc8f5b89d8eba4bfd49
Mark Velez <mark.velez129@gmail.com>
2024-07-20 20:15:17 +0200

    Added command parsing

--- addons/simply-console/singletons/console_data_manager.gd
+++ addons/simply-console/singletons/console_data_manager.gd
@@ -1,11 +1,37 @@
 extends Node
 
+# GLOBAL = Singleton(Autorun)
+# LOCAL = Scene Node
+enum CommandType {
+	GLOBAL,
+	LOCAL
+}
+
 # Command list file location constants
 const FILE: String = "command_list.json"
 const DIRECTORY: String = "res://addons/simply-console/data/"
 const PATH: String = DIRECTORY + FILE
 
-var COMMAND_LIST_: Dictionary
+var COMMAND_LIST_: Dictionary = {
+	"help": {
+		"target": "ConsoleDataManager",
+		"type": CommandType.GLOBAL,
+		"method": "show_command_list",
+		"args": null
+	},
+	"clear": {
+		"target": null,
+		"type": CommandType.LOCAL,
+		"method": "clear_console",
+		"args": null
+	},
+	"test": {
+		"target": "LocalNode",
+		"type": CommandType.LOCAL,
+		"method": "test_method",
+		"args": null
+	}
+}
 
 
 func _ready() -> void:
@@ -22,7 +48,7 @@ func _ready() -> void:
 func save_data() -> void:
 	var file := FileAccess.open(PATH, FileAccess.WRITE)
 	
-	file.store_string(JSON.stringify(COMMAND_LIST_))
+	file.store_string(JSON.stringify(COMMAND_LIST_, "\t"))
 	file.close()
 
 
@@ -36,3 +62,13 @@ func get_data() -> void:
 	if COMMAND_LIST_ == null:
 		push_error("Failed to parse command list file.")
 		return
+
+
+func show_command_list() -> String:
+	var list: String = "List of available commands:\n"
+	
+	for command in COMMAND_LIST_:
+		list += command + ", "
+	
+	list.trim_suffix(", ")
+	return list
