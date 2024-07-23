db044c9f072d3c8207c2d1c504530576812a1e19
Mark Velez <mark.velez129@gmail.com>
2024-07-23 19:50:12 +0200

    Refactoring

--- addons/simply-console/scripts/command_parser.gd
+++ addons/simply-console/scripts/command_parser.gd
@@ -2,59 +2,63 @@ extends Node
 class_name CommandParser
 
 
-func parse_command(command: String, ARGS_: Array = []) -> String:
+func parse_command(command: String, ARGUMENTS_: Array = []) -> String:
 	if not ConsoleDataManager.COMMAND_LIST_.has(command):
 		return "Command '" + command + "' does not exist."
 	
 	var response
 	var TargetRef: Node = get_command_target(command)
 	var method: String = ConsoleDataManager.COMMAND_LIST_[command]["method"]
-	var METHOD_ARGS_: Array = ConsoleDataManager.COMMAND_LIST_[command]["args"]
+	var METHOD_ARGUMENTS_: Array =\
+		ConsoleDataManager.COMMAND_LIST_[command]["argumentList"]
 	
 	if not TargetRef:
 		return "Could not find command target for '" + command + "'."
 	
 	# Check if command expects arguments
-	if args_optional(METHOD_ARGS_, ARGS_):
+	if arguments_optional(METHOD_ARGUMENTS_, ARGUMENTS_):
 		response = TargetRef.call(method)
 		if response:
 			return response
 		return ""
 	
-	if ARGS_.size() > METHOD_ARGS_.size():
+	if ARGUMENTS_.size() > METHOD_ARGUMENTS_.size():
 		return (
 			"Too many arguments for command '"
 			+ command
 			+ "'. Expected "
-			+ str(METHOD_ARGS_.size())
+			+ str(METHOD_ARGUMENTS_.size())
 			+ ", but got "
-			+ str(ARGS_.size())
+			+ str(ARGUMENTS_.size())
 			+ "." 
 		)
 	
-	if not METHOD_ARGS_.is_empty() and ARGS_.is_empty():
+	if not METHOD_ARGUMENTS_.is_empty() and ARGUMENTS_.is_empty():
 		return (
 			"Too few arguments for command '"
 			+ command
 			+ "'. Expected "
-			+ str(METHOD_ARGS_.size())
+			+ str(METHOD_ARGUMENTS_.size())
 			+ ", but got "
-			+ str(ARGS_.size())
+			+ str(ARGUMENTS_.size())
 			+ "." 
 		)
 	
-	var PARSED_ARGS_: Dictionary = parse_args(METHOD_ARGS_, ARGS_)
+	var PARSED_ARGUMENTS_: Dictionary =\
+		parse_argument_list(METHOD_ARGUMENTS_, ARGUMENTS_)
 	
-	if PARSED_ARGS_.has("invalidArg"):
+	if PARSED_ARGUMENTS_.has("invalidArgument"):
 		return (
 			"Invalid type for argument "
-			+ str(PARSED_ARGS_["invalidArg"])
+			+ str(PARSED_ARGUMENTS_["invalidArgument"])
 			+ " expected '"
-			+ type_string(METHOD_ARGS_[PARSED_ARGS_["invalidArg"]]["type"])
+			+ type_string(
+				METHOD_ARGUMENTS_[PARSED_ARGUMENTS_["invalidArgument"]]["type"]
+			)
 			+ "'."
 		)
 	
-	response = TargetRef.callv(method, PARSED_ARGS_["args"])
+	response = TargetRef.callv(method, PARSED_ARGUMENTS_["argumentList"])
 	if response:
 		return response
 	return ""
@@ -77,55 +81,54 @@ func get_command_target(command: String) -> Node:
 	return null
 
 
-func args_optional(METHOD_ARGS_: Array, ARGS_: Array) -> bool:
-	if not ARGS_.is_empty():
+func arguments_optional(METHOD_ARGUMENTS_: Array, ARGUMENTS_: Array) -> bool:
+	if not ARGUMENTS_.is_empty():
 		return false
 	
-	if METHOD_ARGS_.is_empty():
+	if METHOD_ARGUMENTS_.is_empty():
 		return true
 	
-	for ARG_INFO_ in METHOD_ARGS_:
-		if not ARG_INFO_["optional"]:
+	for ARGUMENT_ in METHOD_ARGUMENTS_:
+		if not ARGUMENT_["optional"]:
 			return false
 	
 	return true
 
 
-func parse_args(METHOD_ARGS_: Array, ARGS_: Array) -> Dictionary:
-	var PARSED_ARGS_: Dictionary = {
-		"args": []
+func parse_argument_list(METHOD_ARGUMENTS_: Array, ARGUMENTS_: Array) -> Dictionary:
+	var PARSED_ARGUMENTS_: Dictionary = {
+		"argumentList": []
 	}
 	var i: int = 0
-	var invalidArg: int = 0
+	var invalidArgument: int = 0
 	
-	for ARG_INFO_ in METHOD_ARGS_:
-		var receivedArg: String = ARGS_[i]
+	for ARGUMENT_ in METHOD_ARGUMENTS_:
+		var argument: String = ARGUMENTS_[i]
 		
-		match int(ARG_INFO_["type"]):
+		match int(ARGUMENT_["type"]):
 			TYPE_STRING:
-				PARSED_ARGS_["args"].append(receivedArg)
+				PARSED_ARGUMENTS_["argumentList"].append(argument)
 			
 			TYPE_INT:
-				if receivedArg.is_valid_int():
-					PARSED_ARGS_["args"].append(int(receivedArg))
+				if argument.is_valid_int():
+					PARSED_ARGUMENTS_["argumentList"].append(int(argument))
 				else:
-					invalidArg = i + 1
+					invalidArgument = i + 1
 					break
 			
 			TYPE_FLOAT:
-				if receivedArg.is_valid_float():
-					PARSED_ARGS_["args"].append(float(receivedArg))
+				if argument.is_valid_float():
+					PARSED_ARGUMENTS_["argumentList"].append(float(argument))
 				else:
-					invalidArg = i + 1
+					invalidArgument = i + 1
 					break
 		
-		if i == ARGS_.size() - 1:
+		if i == ARGUMENTS_.size() - 1:
 			break
 		
 		i += 1
 		
-	if invalidArg > 0:
-		PARSED_ARGS_["invalidArg"] = invalidArg
-		return PARSED_ARGS_
+	if invalidArgument > 0:
+		PARSED_ARGUMENTS_["invalidArgument"] = invalidArgument
 	
-	return PARSED_ARGS_
+	return PARSED_ARGUMENTS_
--- addons/simply-console/singletons/console_data_manager.gd
+++ addons/simply-console/singletons/console_data_manager.gd
@@ -18,7 +18,7 @@ var COMMAND_LIST_: Dictionary = {
 		"target": "ConsoleDataManager",
 		"type": CommandType.GLOBAL,
 		"method": "show_command_list",
-		"args": [
+		"argumentList": [
 			{
 				"name": "command",
 				"type": TYPE_STRING,
@@ -30,7 +30,7 @@ var COMMAND_LIST_: Dictionary = {
 		"target": "",
 		"type": CommandType.LOCAL,
 		"method": "clear_console",
-		"args": []
+		"argumentList": []
 	},
 }
 
@@ -67,26 +67,28 @@ func get_data() -> void:
 		return
 
 
-func show_command_list(command: String = "") -> String:
+func show_command_list(commandName: String = "") -> String:
 	var response: String = "List of available commands:\n"
 	
-	if command == "":
-		for cmd in COMMAND_LIST_:
-			response += cmd + ", "
+	# Show list of commands
+	if commandName == "":
+		for command in COMMAND_LIST_:
+			response += command + ", "
 		
 		return response.trim_suffix(", ")
 	
-	if COMMAND_LIST_.has(command):
-		if COMMAND_LIST_[command]["args"].is_empty():
-			return "Command '" + command + "' does not have any arguments."
+	# Show list of arguments for command
+	if COMMAND_LIST_.has(commandName):
+		if COMMAND_LIST_[commandName]["argumentList"].is_empty():
+			return "Command '" + commandName + "' does not have any arguments."
 		
-		response = "Argument(s) for '" + command + "':\n"
-		for ARG_ in COMMAND_LIST_[command]["args"]:
-			response += ARG_["name"]
-			if ARG_["optional"]:
+		response = "Argument(s) for '" + commandName + "':\n"
+		for ARGUMENT_ in COMMAND_LIST_[commandName]["argumentList"]:
+			response += ARGUMENT_["name"]
+			if ARGUMENT_["optional"]:
 				response += " (optional)"
 			response += "\n"
 		
 		return response.trim_suffix("\n")
 	
-	return "Command '" + command + "' does not exist."
+	return "Command '" + commandName + "' does not exist."
